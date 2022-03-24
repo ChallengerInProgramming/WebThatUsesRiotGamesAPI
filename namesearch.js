62576d06e909af65ca70f95f22cbf7ae8a06cbc3
ChallengerInProgramming <tskwon@ucdavis.edu>
2022-03-24 16:56:22 -0700

    Name is now able to be taken from text box and value given from link

--- namesearch.js
+++ namesearch.js
@@ -4,21 +4,37 @@ const riotKey = 'api_key=RGAPI-239042c3-ff1a-4784-8d53-8d7664ee5343';
 //    let url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/High%20Score?api_key=RGAPI-239042c3-ff1a-4784-8d53-8d7664ee5343"
 
 //}
-//testing
-const URL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/HighScore?api_key=RGAPI-239042c3-ff1a-4784-8d53-8d7664ee5343';
 
 
-function nameFunction(){
 
-    fetch(URL)
-    .then(res => res.text())
-    .then(text => {
-    console.log(text);
-  })
-   .catch(err => console.log(err));
+//const URL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/HighScore?api_key=RGAPI-f478804a-4e03-4d80-99f9-f3d2d3892bd8';
+var URL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name';
+const api_key = 'api_key=RGAPI-f478804a-4e03-4d80-99f9-f3d2d3892bd8';
+// percentTwenty is put in empty spaces between names eg "Tom Kwon" would equal "Tom%20Kwon"
+const percentTwenty = '%20';
 
-   var nameValue = document.getElementById('fname').value;
-   document.getElementById("SummonerNameSearchButton").innerHTML = "hi";
-   //document.getElementById("SummonerNameSearchButton").innerHTML = nameValue;
+//placeholder name
+var summonerName = "High Score";
+
+function getValueFromTextBoxFunction(){
+  summonerName = document.getElementById('fname').value;
+  let summonerNameComplete = summonerName.replace(/ /g,percentTwenty);
+  URL = URL + '/' + summonerNameComplete + '?' + api_key;
+  document.getElementById("SummonerNameSearchButton").innerHTML = URL;
+  console.log(URL);
 }
+
+// function nameFunction(){
+
+//     fetch(URL)
+//     .then(res => res.text())
+//     .then(text => {
+//     console.log(text);
+//   })
+//    .catch(err => console.log(err));
+
+//    var nameValue = document.getElementById('fname').value;
+//    document.getElementById("SummonerNameSearchButton").innerHTML = "hi";
+//    //document.getElementById("SummonerNameSearchButton").innerHTML = nameValue;
+// }
 
