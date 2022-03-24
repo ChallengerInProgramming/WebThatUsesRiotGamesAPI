87635fade1182a5ee98b6d0e7fd03d409806600f
ChallengerInProgramming <97718650+ChallengerInProgramming@users.noreply.github.com>
2022-03-24 15:29:41 -0700

    Add files via upload

---
+++ namesearch.js
@@ -0,0 +1,24 @@
+const riotKey = 'api_key=RGAPI-239042c3-ff1a-4784-8d53-8d7664ee5343';
+
+//function getValues(){
+//    let url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/High%20Score?api_key=RGAPI-239042c3-ff1a-4784-8d53-8d7664ee5343"
+
+//}
+
+const URL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/HighScore?api_key=RGAPI-239042c3-ff1a-4784-8d53-8d7664ee5343';
+
+
+function nameFunction(){
+
+    fetch(URL)
+    .then(res => res.text())
+    .then(text => {
+    console.log(text);
+  })
+   .catch(err => console.log(err));
+
+   var nameValue = document.getElementById('fname').value;
+   document.getElementById("SummonerNameSearchButton").innerHTML = "hi";
+   //document.getElementById("SummonerNameSearchButton").innerHTML = nameValue;
+}
+
