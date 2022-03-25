2ab0dfcea685a3a3b77afc632be65472a5e5bc45
ChallengerInProgramming <tskwon@ucdavis.edu>
2022-03-24 20:51:08 -0700

    can access the contents of match history

--- namesearch.js
+++ namesearch.js
@@ -1,14 +1,9 @@
 const riotKey = 'api_key=RGAPI-239042c3-ff1a-4784-8d53-8d7664ee5343';
 
-//function getValues(){
-//    let url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/High%20Score?api_key=RGAPI-239042c3-ff1a-4784-8d53-8d7664ee5343"
+var SummonerURL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name';
+var MatchURL = 'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/';
+var MatchDetailsURL = 'https://americas.api.riotgames.com/lol/match/v5/matches/'
 
-//}
-
-
-
-//const URL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/HighScore?api_key=RGAPI-f478804a-4e03-4d80-99f9-f3d2d3892bd8';
-var URL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name';
 const api_key = 'api_key=RGAPI-f478804a-4e03-4d80-99f9-f3d2d3892bd8';
 // percentTwenty is put in empty spaces between names eg "Tom Kwon" would equal "Tom%20Kwon"
 const percentTwenty = '%20';
@@ -19,15 +14,30 @@ var summonerName = "High Score";
 async function getValueFromTextBoxFunction(){
   summonerName = document.getElementById('fname').value;
   let summonerNameComplete = summonerName.replace(/ /g,percentTwenty);
-  URL = URL + '/' + summonerNameComplete + '?' + api_key;
-  document.getElementById("SummonerNameSearchButton").innerHTML = URL;
-  console.log(URL);
-  let response = await fetch(URL);
+  SummonerURL = SummonerURL + '/' + summonerNameComplete + '?' + api_key;
+  // document.getElementById("SummonerNameSearchButton").innerHTML = URL;
+  //console.log(SummonerURL);
+  let response = await fetch(SummonerURL);
   response = await response.json()
-  console.log(response);
+  //document.getElementById("SummonerNameSearchButton").innerHTML = 'puuid = ' + response.puuid;
+  //console.log(response);
 
+  // this next part will put the puuid and get match ids. Currently it will get only the previous one
+  //https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/aWMgztnpk0uVYRZ_CaPPNS92Y7N6mutpGVyR8O-o_wso0fudWeY6n4KW7ds8n0MWIjiec0MA9eK3qg/ids?start=0&count=20&api_key=RGAPI-f478804a-4e03-4d80-99f9-f3d2d3892bd8
+  MatchURL = MatchURL + response.puuid + '/' + 'ids?start=0&count=20&' + api_key;
+  //document.getElementById("SummonerNameSearchButton").innerHTML = 'matches = ' + MatchURL;
+  let responseMatch = await fetch(MatchURL);
+  responseMatch = await responseMatch.json()
+  //document.getElementById("SummonerNameSearchButton").innerHTML = 'MatchID = ' + responseMatch[0];
 
-  URL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name';
+  // this part will match the matchID with details of the game (see what I did there? MATCH THE MATCHID? :D)
+  MatchDetailsURL = MatchDetailsURL + responseMatch[0] + '?' + api_key;
+  let responseMatchDetailsURL = await fetch(MatchDetailsURL);
+  responseMatchDetailsURL = await responseMatchDetailsURL.json()
+  document.getElementById("SummonerNameSearchButton").innerHTML = 'First Detail = ' + responseMatchDetailsURL.info.gameCreation;
+
+  // this is to reset the URL so that it does not continually add information and get longer and longer
+  SummonerURL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name';
 }
 
 // function nameFunction(){
