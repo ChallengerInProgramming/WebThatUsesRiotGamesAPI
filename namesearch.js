7b780b5fd0415359ffaf7f2614c81dff37aa2b2c
ChallengerInProgramming <tskwon@ucdavis.edu>
2022-03-25 21:58:05 -0700

    figured out what data I want and why

--- namesearch.js
+++ namesearch.js
@@ -1,10 +1,12 @@
-const riotKey = 'api_key=RGAPI-239042c3-ff1a-4784-8d53-8d7664ee5343';
+// IDEAS: Get cs lead over oponent. Get death count. Get damage difference between oponent. Get gank amount compared to opnnent. 
+// Idea seems to be focus on the important differences between yourself and your oponent.
+// Idea is basically like other sites like OPGG or Mobalytics except focus entierley on yourself and the person playign the same role on the other side.
 
 var SummonerURL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name';
 var MatchURL = 'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/';
 var MatchDetailsURL = 'https://americas.api.riotgames.com/lol/match/v5/matches/'
 
-const api_key = 'api_key=RGAPI-f478804a-4e03-4d80-99f9-f3d2d3892bd8';
+const api_key = 'api_key=RGAPI-0dcab893-2a72-49f2-a18b-82b80b9bb0de';
 // percentTwenty is put in empty spaces between names eg "Tom Kwon" would equal "Tom%20Kwon"
 const percentTwenty = '%20';
 
@@ -34,7 +36,7 @@ async function getValueFromTextBoxFunction(){
   MatchDetailsURL = MatchDetailsURL + responseMatch[0] + '?' + api_key;
   let responseMatchDetailsURL = await fetch(MatchDetailsURL);
   responseMatchDetailsURL = await responseMatchDetailsURL.json()
-  document.getElementById("SummonerNameSearchButton").innerHTML = 'First Detail = ' + responseMatchDetailsURL.info.gameCreation;
+  //document.getElementById("SummonerNameSearchButton").innerHTML = 'Bounty Level = ' + responseMatchDetailsURL.info.participants[0].bountyLevel;
 
   // this is to reset the URL so that it does not continually add information and get longer and longer
   SummonerURL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name';
